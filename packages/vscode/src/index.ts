7047f49cf67f94729fb8a94e5ef11a384b334146
zhangpaopao <zhangpaopao0609@gmail.com>
2024-05-11 20:10:54 +0800

    feat: vscode snapshot

--- packages/vscode/src/index.ts
+++ packages/vscode/src/index.ts
@@ -1,7 +1,40 @@
-import { window } from 'vscode'
+import { commands, window } from 'vscode'
+
+export interface Snapshot {
+  content: string
+  time: number
+}
 
 export function activate() {
   window.showInformationMessage('Hi')
+
+  const snapMap = new Map<string, Snapshot[]>()
+
+  commands.registerCommand('retypewriter.snap', () => {
+    const doc = window.activeTextEditor?.document
+    if (!doc)
+      return
+    const path = doc.uri.fsPath
+    if (!snapMap.has(path))
+      snapMap.set(path, [])
+    snapMap.get(path)!.push({
+      content: doc.getText(),
+      time: Date.now(),
+    })
+  })
+
+  commands.registerCommand('retypewriter.play', () => {
+    const doc = window.activeTextEditor?.document
+    if (!doc)
+      return
+    const path = doc.uri.fsPath
+    const snaps = snapMap.get(path)
+    if (!snaps?.length)
+      window.showErrorMessage('No snapshots found')
+      // return
+
+    // const lastSnap = snaps[snaps.length - 1]
+  })
 }
 
 export function deactivate() {
